106ae8e482ad7a00e147f1075c4bfdd20feec623
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2020-05-01 21:17:24 +0200

    Navbar initial layout without actions added

--- Android app/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.myapplication.activities.LoginActivity;
 import com.example.myapplication.activities.TestActivity;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.all_tasks);
         System.out.println("main activity ON CREATE");
 
 
