6428a11e2e7f2cb7075464c9559edba85b387afd
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2020-06-08 21:37:41 +0200

    Icons improvement

--- Android app/app/src/main/java/com/example/myapplication/util/AppConfig.java
+++ Android app/app/src/main/java/com/example/myapplication/util/AppConfig.java
@@ -2,5 +2,5 @@ package com.example.myapplication.util;
 
 public class AppConfig {
 
-    public static String apiURI = "http://192.168.0.25:8080/api/";
+    public static String apiURI = "http://192.168.0.29:8080/api/";
 }
