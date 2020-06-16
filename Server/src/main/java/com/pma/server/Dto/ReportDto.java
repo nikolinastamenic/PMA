7c4831702c6a13696445759e639777824d53293e
jovanaristovic <jovanaristovic@uns.ac.rs>
2020-06-16 13:10:03 +0200

    android login done

--- Server/src/main/java/com/pma/server/Dto/ReportDto.java
+++ Server/src/main/java/com/pma/server/Dto/ReportDto.java
@@ -1,13 +1,13 @@
 package com.pma.server.Dto;
 
-import com.pma.server.model.Report;
-import com.pma.server.model.ReportItem;
-import com.pma.server.model.Task;
+
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Data
 public class ReportDto {
 
     private Long id;
@@ -21,27 +21,4 @@ public class ReportDto {
     }
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public List<ReportItemDto> getItemList() {
-        return itemList;
-    }
-
-    public void setItemList(List<ReportItemDto> itemList) {
-        this.itemList = itemList;
-    }
 }
--- Server/src/main/java/com/pma/server/Dto/ReportItemDto.java
+++ Server/src/main/java/com/pma/server/Dto/ReportItemDto.java
@@ -1,51 +1,13 @@
 package com.pma.server.Dto;
 
+import lombok.Data;
 
-import com.pma.server.model.ReportItem;
-
+@Data
 public class ReportItemDto {
 
     private Long id;
-
     private String faultName;
-
     private String details;
-
     private String picture;
 
-    public ReportItemDto() {}
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFaultName() {
-        return faultName;
-    }
-
-    public void setFaultName(String faultName) {
-        this.faultName = faultName;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
 }
